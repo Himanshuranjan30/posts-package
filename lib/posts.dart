315c4459fe218957210e73ac66432535b8d5906c
Himanshu <himanshu@pop-os.localdomain>
2020-11-13 11:14:34 +0530

    example added

--- lib/posts.dart
+++ lib/posts.dart
@@ -1,7 +1,45 @@
 library posts;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  double height;
+  double width;
+  String imageurl;
+  bool isNetworkImage = false;
+
+  Post(
+    this.height,
+    this.width,
+    this.imageurl,
+    this.isNetworkImage,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 1.5,
+      child: Column(children: [
+        isNetworkImage
+            ? Container(
+                child: Image.network(
+                imageurl,
+              ))
+            : Image.asset(imageurl),
+        SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.thumb_up),
+            Icon(Icons.comment),
+            Icon(Icons.share)
+          ],
+        ),
+        Divider(),
+      ]),
+    );
+  }
 }
